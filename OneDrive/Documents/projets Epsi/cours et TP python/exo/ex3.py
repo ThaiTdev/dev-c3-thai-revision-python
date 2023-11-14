894c092e197109670d0d04ad37a3a7fc58297126
cooleydev <t.thai@outlook.fr>
2023-11-14 16:16:07 +0100

    calcule deux nombres en parametre

--- OneDrive/Documents/projets Epsi/cours et TP python/exo/ex3.py
+++ OneDrive/Documents/projets Epsi/cours et TP python/exo/ex3.py
@@ -1,10 +1,31 @@
 def calculatrice(calcul):
-     nb1 = ""
-     nb2 = 0
-     sign = ""
-     for num in calcul:
-        while num.isalpha():
-                nb1 += num
-        print(nb1)   
-       
-calculatrice("28*5")
+    nb1 = ""
+    nb2 = 0
+    sign = ""
+    list_calcul = list(calcul)
+    print(list_calcul) 
+
+    for item in list_calcul:
+        if item.isdigit() or item == '.':
+            nb1 += item
+        elif item in ['+', '-', '*', '/']:
+            sign = item
+            nb2 = float(nb1)
+            nb1 = ""
+
+    nb2 = float(nb1)  # Prend en compte le dernier nombre après le dernier opérateur
+
+    if sign == '+':
+        resultat = nb2 + float(nb1)
+    elif sign == '-':
+        resultat = nb2 - float(nb1)
+    elif sign == '*':
+        resultat = nb2 * float(nb1)
+    elif sign == '/':
+        resultat = nb2 / float(nb1)
+    else:
+        print("Opérateur non valide.")
+        return
+
+    print(f"Résultat du calcul {resultat}") 
+calculatrice("3*9")
