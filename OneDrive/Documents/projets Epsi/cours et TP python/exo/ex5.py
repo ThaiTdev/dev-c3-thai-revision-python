25ef4809e701f52a60e41fb00ff58f0b34f8601e
cooleydev <t.thai@outlook.fr>
2023-11-14 17:19:53 +0100

    ex5 first commit

---
+++ OneDrive/Documents/projets Epsi/cours et TP python/exo/ex5.py
@@ -0,0 +1,13 @@
+def calcul_factoriel(num):
+    if not isinstance(num, int):
+        print("La valeur passée en paramètre de votre fonction n'est pas un nombre entier.")
+        return
+
+    facto = 1
+    for i in range(1, num + 1):
+        facto *= i
+
+    print(f"Le factoriel de {num} est : {facto}")
+
+
+calcul_factoriel(5)
