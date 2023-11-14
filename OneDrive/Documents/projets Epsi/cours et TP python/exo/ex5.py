de59ac967d40e283f2128fbb34d819a82fb5f9a3
cooleydev <t.thai@outlook.fr>
2023-11-14 17:26:02 +0100

    ex6 first commit

--- OneDrive/Documents/projets Epsi/cours et TP python/exo/ex5.py
+++ OneDrive/Documents/projets Epsi/cours et TP python/exo/ex5.py
@@ -7,7 +7,7 @@ def calcul_factoriel(num):
     for i in range(1, num + 1):
         facto *= i
 
-    print(f"Le factoriel de {num} est : {facto}")
+    print(f"Le factoriel de {num} est: {facto}")
 
 
 calcul_factoriel(5)
